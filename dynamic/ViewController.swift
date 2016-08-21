8e5271b934a4633c5bcf5b2bf0d0a8e623123beb
adhoc <adhoc@adhocs-imac.local>
2016-08-21 14:26:41 -0700

    Initial Commit

---
+++ dynamic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dynamic
+//
+//  Created by adhoc on 18/11/1437 AH.
+//  Copyright © 1437 AH ammach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
